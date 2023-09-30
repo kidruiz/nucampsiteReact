5cb09cf1459ed3169d69503d831f361b33f96633
Carlos Ruiz <carloshruiz90@gmail.com>
2023-10-01 08:12:54 +1000

    addComment

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -16,10 +16,6 @@ export const selectAllCampsites = (state) => {
     return state.campsites.campsitesArray;
 };
 
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
-
 export const selectCampsiteById = (id) => (state) => {
     return state.campsites.campsitesArray.find(
         (campsite) => campsite.id === parseInt(id)
