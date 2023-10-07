d55a36c00d04b4ec1605bfdec1327aac1b430c62
Carlos Ruiz <carloshruiz90@gmail.com>
2023-10-07 21:56:37 +1000

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsiteDetail from './features/campsites/CampsiteDetail';
+//import CampsiteDetail from './features/campsites/CampsiteDetail';
 import './App.css';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
-  return (
-    <div className='App'>
-        <Header />
-        <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='contact' element={<ContactPage />} />
-            <Route path='directory' element={<CampsitesDirectoryPage />} />
-            <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-            <Route path='/about' element={<AboutPage />} />
-        </Routes>
-        <Footer />
-    </div>
-  );
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+                <Route path='/about' element={<AboutPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
