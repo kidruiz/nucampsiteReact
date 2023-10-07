e4dd9379ab793fe9a7240e73b6e13f2f2eb97616
Carlos Ruiz <carloshruiz90@gmail.com>
2023-10-08 01:34:43 +1000

    Async Partners

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import './App.css';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 
 function App() {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
     return (
